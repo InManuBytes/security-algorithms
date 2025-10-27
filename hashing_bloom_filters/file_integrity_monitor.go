377b851c819467cc3ee08f8fa372b3b7bf644160
Manu Gomez <manu.gomez@cockroachlabs.com>
2025-10-27 13:17:07 -0400

    add fixed check

--- hashing_bloom_filters/file_integrity_monitor.go
+++ hashing_bloom_filters/file_integrity_monitor.go
@@ -12,12 +12,14 @@
 package main
 
 import (
-	"fmt"
-	"encoding/json"
-	"os"
-	"io"
+	"bytes"
 	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"os"
 )
 
 const (
@@ -33,6 +35,11 @@ func main() {
 		"config/settings.yaml": "320df0b959781a7b12b304edd9431559900d67925a9b275d3ac0a55ad81b0c6a"
 	}`)
 	verifyIntegrity(manifestJsonBlob)
+
+	// tighter
+	if ok := verifyIntegrityFixed(manifestJsonBlob); !ok {
+		os.Exit(1)
+	}
 }
 
 func verifyIntegrity(input []byte) {
@@ -55,7 +62,7 @@ func verifyIntegrity(input []byte) {
 			// io.Copy handles large files by reading in chunks
 			log.Fatal(err)
 		}
-		// hash.Sum(nil) return a byte slice as hexadecimal
+		// hash.Sum(nil) return a byte slice
 		// %x is a base 16 integer
 		// NOTE: while this works, it can be vulnerable to timing attacks
 		if checksum != fmt.Sprintf("%x", hash.Sum(nil)) {
@@ -66,19 +73,29 @@ func verifyIntegrity(input []byte) {
 	}
 }
 
-func verifyIntegrityFixed(input []byte) {
+func verifyIntegrityFixed(input []byte) bool {
 	var files map[string]string
 	err := json.Unmarshal(input, &files)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for file, checksum := range files {
-		file, err := os.Open(file) // file is a reader
-		if err != nil {
-			log.Fatal(err)
+	allOk := true
+
+	for filePath, checksum := range files { // careful with shadow variables
+		expectedHash, err := hex.DecodeString(checksum)
+		if err != nil || len(checksum) != sha256.Size {
+			log.Printf("ERROR: %s (invalid expected SHA256 hex)", filePath)
+			allOk = false
+			continue
 		}
-		defer file.Close()
+
+		file, err := os.Open(filePath) // file is a reader
+		if err != nil {
+			log.Printf("ERROR: %s", err) // alternatively you can log and error and continue
+			allOk = false
+			continue
+		}
 
 		// from the example in the docs
 		hash := sha256.New()
@@ -86,9 +103,22 @@ func verifyIntegrityFixed(input []byte) {
 			// io.Copy handles large files by reading in chunks
 			log.Fatal(err)
 		}
+		// runs on each iteration to avoid "too many open files"
+		if cerr := file.Close(); cerr != nil {
+			log.Printf("WARN: %s (close fialed: %v)", filePath, err)
+		}
 
-		// TODO fix naive check
+		computedHash := hash.Sum(nil)
+		// if you don't need constant time semantics use
+		if !bytes.Equal(computedHash, expectedHash) {
+			log.Printf(ChecksumMismatch, filePath)
+			allOk = false
+			continue
+		}
+
+		log.Printf(ChecksumMatch, filePath)
+
 	}
 
-	return
+	return allOk
 }
